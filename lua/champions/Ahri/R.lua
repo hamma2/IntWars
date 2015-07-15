e727dd896d928de3a9844afaea8d609d6a68660f
joaquin95 <joaquin95@users.noreply.github.com>
2015-07-15 18:49:42 -0300

    Ahri's R
    Works like crap

---
+++ lua/champions/Ahri/R.lua
@@ -0,0 +1,12 @@
+Vector2 = require 'Vector2' -- include 2d vector lib 
+
+function finishCasting()    
+    local current = Vector2:new(getOwnerX(), getOwnerY())
+    local to = (Vector2:new(getSpellToX(), getSpellToY()) - current):normalize()
+    local range = to * 450
+    local trueCoords = current + range    
+    dashTo(getOwner(), trueCoords.x, trueCoords.y, 1200)
+end
+
+function applyEffects()
+end
